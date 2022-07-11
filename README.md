80f22603b28c5a487cf3d41a54f5023ea174967b
StivyPada1 <stivy44@gmail.com>
2022-07-11 22:56:32 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-cxvsjq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-cxvsjq)
---
+++ index.js
@@ -0,0 +1,14 @@
+// run `node index.js` in the terminal
+
+var http = require("http");
+
+//create a server object:
+http
+  .createServer(function(req, res) {
+    res.write("Hello World!"); //write a response to the client
+    res.end(); //end the response
+  })
+  .listen(8080); //the server object listens on port 8080
+
+
+//console.log(`Hello Node.js v${process.versions.node}!`);
